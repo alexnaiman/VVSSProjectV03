33778c293e4434b172c37fe2999485330b82c2a4
Alexandra Nistor <Alexandra.Nistor@cloudflight.io>
2020-03-13 20:12:13 +0200

    Change Junit 5 to 4.12

--- test/TestAddStudent.java
+++ test/TestAddStudent.java
@@ -2,30 +2,36 @@ import VVSSProjectV03MV.Exceptions.ValidatorException;
 import VVSSProjectV03MV.Repository.XMLFileRepository.StudentXMLRepo;
 import VVSSProjectV03MV.Service.XMLFileService.StudentXMLService;
 import VVSSProjectV03MV.Validator.StudentValidator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-public class TestAddStudent{
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TestAddStudent {
 
     @Test
     public void testAddStudentValidInput() throws ValidatorException {
 
-        StudentValidator vs=new StudentValidator();
-        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
-        StudentXMLService stsrv=new StudentXMLService(strepo);
+        StudentValidator vs = new StudentValidator();
+        StudentXMLRepo strepo = new StudentXMLRepo(vs, "StudentiXML.xml");
+        StudentXMLService stsrv = new StudentXMLService(strepo);
         String[] params = {"1", "Alexandru", "935", "naie2371@scs.ubbcluj.ro", "Lorincz Beata"};
         stsrv.add(params);
-        Assertions.assertEquals(strepo.size(), 1);
+        assertEquals(strepo.size(), 1);
     }
 
     @Test
     public void testAddStudentInvalidGroupInput() {
 
-        StudentValidator vs=new StudentValidator();
-        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
-        StudentXMLService stsrv=new StudentXMLService(strepo);
+        StudentValidator vs = new StudentValidator();
+        StudentXMLRepo strepo = new StudentXMLRepo(vs, "StudentiXML.xml");
+        StudentXMLService stsrv = new StudentXMLService(strepo);
         String[] params = {"1", "Alexandru", "93a", "naie2371@scs.ubbcluj.ro", "Lorincz Beata"};
-        Assertions.assertThrows(ValidatorException.class, () -> stsrv.add(params));
+        try {
+            stsrv.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (ValidatorException exception) {
 
+        }
     }
 }
