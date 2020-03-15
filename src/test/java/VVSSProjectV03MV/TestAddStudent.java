42288d16b06aa0523e865532462fc29be4195d5b
Alexandra Nistor <Alexandra.Nistor@cloudflight.io>
2020-03-15 12:57:11 +0200

    Small change to tests

--- src/test/java/VVSSProjectV03MV/TestAddStudent.java
+++ src/test/java/VVSSProjectV03MV/TestAddStudent.java
@@ -4,6 +4,7 @@ import VVSSProjectV03MV.Exceptions.ValidatorException;
 import VVSSProjectV03MV.Repository.XMLFileRepository.StudentXMLRepo;
 import VVSSProjectV03MV.Service.XMLFileService.StudentXMLService;
 import VVSSProjectV03MV.Validator.StudentValidator;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,29 +12,34 @@ import static org.junit.Assert.fail;
 
 public class TestAddStudent {
 
+    private StudentValidator validator;
+    private StudentXMLRepo repository;
+    private StudentXMLService service;
+
+    @Before
+    public void setup() {
+        this.validator = new StudentValidator();
+        this.repository = new StudentXMLRepo(validator, "StudentiXML.xml");
+        this.service = new StudentXMLService(repository);
+    }
+
     @Test
     public void testAddStudentValidInput() throws ValidatorException {
 
-        StudentValidator vs = new StudentValidator();
-        StudentXMLRepo strepo = new StudentXMLRepo(vs, "StudentiXML.xml");
-        StudentXMLService stsrv = new StudentXMLService(strepo);
         String[] params = {"1", "Alexandru", "935", "naie2371@scs.ubbcluj.ro", "Lorincz Beata"};
-        stsrv.add(params);
-        assertEquals(strepo.size(), 1);
+        this.service.add(params);
+        assertEquals(this.repository.size(), 1);
     }
 
     @Test
     public void testAddStudentInvalidGroupInput() {
 
-        StudentValidator vs = new StudentValidator();
-        StudentXMLRepo strepo = new StudentXMLRepo(vs, "StudentiXML.xml");
-        StudentXMLService stsrv = new StudentXMLService(strepo);
         String[] params = {"1", "Alexandru", "93a", "naie2371@scs.ubbcluj.ro", "Lorincz Beata"};
         try {
-            stsrv.add(params);
+            this.service.add(params);
             fail("Method didn't throw when it was expected to");
         } catch (ValidatorException exception) {
-
+            System.out.println(exception.getMessage());
         }
     }
 }
