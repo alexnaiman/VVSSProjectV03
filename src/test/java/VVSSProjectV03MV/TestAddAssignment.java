ccb205389591abb231185ec82487ce2ff9e15e2c
Alex Naiman <alex.naiman.4@gmail.com>
2020-04-07 22:07:47 +0300

    - tests for lab

--- src/test/java/VVSSProjectV03MV/TestAddAssignment.java
+++ src/test/java/VVSSProjectV03MV/TestAddAssignment.java
@@ -23,14 +23,14 @@ public class TestAddAssignment {
     @Before
     public void setup() {
         this.validator = new TemaLabValidator();
-        this.repository = new TemaLabXMLRepo(validator,"TemaLaboratorXML.xml");
+        this.repository = new TemaLabXMLRepo(validator, "TemaLaboratorXML.xml");
         this.service = new TemaLabXMLService(repository);
     }
 
     @Test
     public void testAddAssignmentValidInput() throws ValidatorException {
 
-        String[] params = {"1", "homework-description", "3", "2"};
+        String[] params = {"6", "description", "8", "6"};
         this.service.add(params);
         assertEquals(this.repository.size(), 1);
     }
@@ -45,4 +45,127 @@ public class TestAddAssignment {
             System.out.println(exception.getMessage());
         }
     }
+
+    @Test
+    public void testAddAssignment1() {
+        String[] params = {null, null, null, null};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment2() {
+        String[] params = {"", "description", "8", "6"};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment3() {
+        String[] params = {"1", "", "8", "6"};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment4() {
+        String[] params = {"1", "description", "2", "0"};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment5() {
+        String[] params = {"1", "description", "15", "10"};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment6() {
+        String[] params = {"1", "description", "8", "6"};
+        try {
+            this.service.add(params);
+//            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment7() {
+        String[] params = {"2", "description", "8", "6"};
+        try {
+            this.service.add(params);
+//            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment8() {
+        String[] params = {"3", "description", "8", "6"};
+        try {
+            this.service.add(params);
+//            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment9() {
+        String[] params = {"4", "description", "8", "6"};
+        try {
+            this.service.add(params);
+//            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment10() {
+        String[] params = {"5", "description", "0", "2"};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignment11() {
+        String[] params = {"6", "description", "8", "15"};
+        try {
+            this.service.add(params);
+            fail("Method didn't throw when it was expected to");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+
 }
